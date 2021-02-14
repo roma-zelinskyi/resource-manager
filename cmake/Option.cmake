623a95855a93ff320ffde2a83d028132057ef987
Roman Zelinskyi <lord.zelinskyi@gmail.com>
2021-02-14 23:28:21 +0200

    build: Add default release build type
    Added option that checks if build type was set
    and if it is not then set it to Release.
    
    Signed-off-by: Roman Zelinskyi <lord.zelinskyi@gmail.com>

--- cmake/Option.cmake
+++ cmake/Option.cmake
@@ -2,4 +2,16 @@
 #
 # @author  Roman Zelinskyi <lord.zelinskyi@gmail.com>
 
-option(TESTS "Enable/disable the tests" ON)
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    option(TESTS "Enable/disable tests" ON)
+endif()
+
+function(printOption name value)
+    if(${value})
+        message(STATUS "${name}: ON")
+    else()
+        message(STATUS "${name}: OFF")
+    endif()
+endfunction()
+
+printOption("Tests" TESTS)
