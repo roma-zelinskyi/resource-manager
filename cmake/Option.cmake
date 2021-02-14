08078d3565cdc0f89321cfd0f32ce71c5f48da91
Roman Zelinskyi <lord.zelinskyi@gmail.com>
2021-02-15 01:49:25 +0200

    improvement: Format CMake files according to config
    Signed-off-by: Roman Zelinskyi <lord.zelinskyi@gmail.com>

--- cmake/Option.cmake
+++ cmake/Option.cmake
@@ -3,15 +3,15 @@
 # @author  Roman Zelinskyi <lord.zelinskyi@gmail.com>
 
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    option(TESTS "Enable/disable tests" ON)
+  option(TESTS "Enable/disable tests" ON)
 endif()
 
-function(printOption name value)
-    if(${value})
-        message(STATUS "${name}: ON")
-    else()
-        message(STATUS "${name}: OFF")
-    endif()
+function(print_option name value)
+  if(${value})
+    message(STATUS "${name}: ON")
+  else()
+    message(STATUS "${name}: OFF")
+  endif()
 endfunction()
 
-printOption("Tests" TESTS)
+print_option("Tests" TESTS)
